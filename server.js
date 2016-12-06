d184dc32e749f310ee3881dfa059b42552fc2bad
Tyler <tyler@eeosk.com>
2016-12-06 11:44:11 -0800

    Initial component working

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+process.env.NODE_ENV = 'development'
+process.env.PORT = 5555
+
+let express     = require('express')
+,   serveStatic = require('serve-static')
+,   compression = require('compression')
+,   path        = require('path')
+,   app = express()
+
+app.use(compression())
+
+app.get('/bootstrap/:component', (req, res, next) => {
+  // res.sendFile(req.params.component + '.html', { root: path.join(__dirname, 'pages/bootstrap') })
+  try {
+    res.render(path.join(__dirname, 'pages/index.ejs'), {
+      path: 'bootstrap/' + req.params.component + '.html'
+    })
+  } catch(err) {
+    res.send(err)
+  }
+})
+
+app.use(serveStatic(path.join(__dirname, '')))
+
+app.listen(process.env.PORT, () => {
+  console.log('Example app listening on port ' + process.env.PORT)
+})
